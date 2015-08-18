b03c9d3a6d7f24b6938d803aed83b6df34c7d224
librandon <16lib62@millshighsch.org>
2015-08-18 09:19:09 -0700

    b

---
+++ brandon/Hello_World.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Hello_World here.
+ * 
+ * Brandon Li
+ * Aug 17, 2015
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Brandon");
+//Display the string
+    }
+}
