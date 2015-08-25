4d4c62ce31cd99ced90f77f971f7f5d7395b25c7
librandon <16lib62@millshighsch.org>
2015-08-25 09:26:42 -0700

    8/25/15

---
+++ folder/Mississippi.java
@@ -0,0 +1,90 @@
+public class Mississippi
+{
+    public static void main(String[] args)
+    {
+        M();
+        I();
+        S();
+        S();
+        I();
+        I();
+        S();
+        S();
+        I();
+        P();
+        P();
+        I();
+    }
+    public static void M()
+    {
+        MM();
+        System.out.println("MM   MM");
+        System.out.println("M M M M");
+        System.out.println("M  M  M");
+        MM();
+        MM();
+        MM();
+    }
+    public static void I()
+    {
+        IBar();
+        Is();
+        Is();
+        Is();
+        Is();
+        Is();
+        IBar();
+    }
+    public static void S()
+    {
+        SBar();
+        Ss();
+        System.out.println("S      ");
+        SBar();
+        System.out.println("      S");
+        Ss();
+        SBar();
+    }
+    public static void P()
+    {
+        PBar();
+        Pp();
+        Pp();
+        PBar();
+        p();
+        p();
+        P();
+    }
+    public static void MM()
+    {
+        System.out.println("M     M");
+    }
+    public static void IBar()
+    {
+        System.out.println("IIIII");
+    }
+    public static void Is()
+    {
+        System.out.println("  I  ");
+    }
+    public static void SBar()
+    {
+        System.out.println(" SSSSS ");
+    }
+    public static void Ss()
+    {
+        System.out.println("S     S");
+    }
+    public static void PBar()
+    {
+        System.out.println("PPPPPP ");
+    }
+    public static void Pp()
+    {
+        System.out.println("P     P");
+    }
+    public static void p()
+    {
+        System.out.println("P      ");
+    } 
+}
